13ea8963f9e5694a1818fc98cdf578b653e91837
K.Utsunomiya <kii.utsunomiya@gmail.com>
2023-07-11 22:29:48 +0900

    Update deps: eslint, eslint-config-prettier Add deps: eslint-plugin-node, eslint-plugin-regexp Delete deps: eslint-config-oclif, eslint-config-oclif-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,35 @@
 module.exports = {
-  extends: ['oclif', 'oclif-typescript', 'prettier'],
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  plugins: ['regexp', 'node', '@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:regexp/recommended', 'plugin:node/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
   rules: {
+    'node/shebang': 0,
     semi: 'off',
-    'valid-jsdoc': 'off',
-    'unicorn/no-hex-escape': 'off',
-    'unicorn/escape-case': 'off',
-    'no-useless-constructor': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-unused-expressions': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-missing-import': [
+      'error',
+      {
+        tryExtensions: ['.js', '.ts'],
+      },
+    ],
   },
 }
